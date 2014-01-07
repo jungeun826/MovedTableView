8465f1f1dbd11c8bd07c7c1d8e385a470069e432
jungeun826 <jungeun826@nate.com>
2014-01-07 19:57:05 +0900

    실습 188페이지를 완료했다.

--- MovedTableView/ViewController.m
+++ MovedTableView/ViewController.m
@@ -1,23 +1,74 @@
 //
 //  ViewController.m
-//  MovedTableView
+//  editingTableView
 //
-//  Created by SDT-1 on 2014. 1. 7..
+//  Created by SDT-1 on 2014. 1. 6..
 //  Copyright (c) 2014년 SDT-1. All rights reserved.
 //
 
 #import "ViewController.h"
+#define CELL_ID @"CELL_ID"
+@interface ViewController ()<UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate>{
+    NSMutableArray *data;
+}
+@property (weak, nonatomic) IBOutlet UITableView *table;
+@property (weak, nonatomic) IBOutlet UITextField *userInput;
 
-@interface ViewController ()
 
 @end
 
 @implementation ViewController
-
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [data count];
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CELL_ID forIndexPath:indexPath];
+    cell.textLabel.text = data[indexPath.row];
+    
+    return cell;
+}
+-(void)tableView:tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    if(editingStyle == UITableViewCellEditingStyleDelete){
+        NSLog(@"%d번째 삭제", (int)indexPath.row);
+        [data removeObjectAtIndex:indexPath.row];
+        NSArray *rowList = [NSArray arrayWithObject:indexPath];
+        [tableView deleteRowsAtIndexPaths:rowList withRowAnimation:UITableViewRowAnimationAutomatic];
+    }
+}
+-(BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath{
+    return YES;
+}
+-(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath{
+    NSObject *obj = data[sourceIndexPath.row];
+    [data removeObjectAtIndex:sourceIndexPath.row];
+    [data insertObject:obj atIndex:destinationIndexPath.row];
+}
+- (IBAction)addItem:(id)sender {
+    NSString *inputStr = self.userInput.text;
+    if([inputStr length] >0){
+        [data addObject:inputStr];
+        NSIndexPath *indexPath = [NSIndexPath indexPathForRow:([data count]-1) inSection:0];
+        NSArray *row = [NSArray arrayWithObject:indexPath];
+        [self.table insertRowsAtIndexPaths:row withRowAnimation:UITableViewRowAnimationAutomatic];
+        
+        self.userInput.text = @"";
+    }
+}
+- (IBAction)toggleEditMode:(id)sender {
+    self.table.editing = !self.table.editing;
+    ((UIBarButtonItem *)sender).title = self.table.editing ? @"Done" : @"Edit";
+}
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    [self addItem:nil];
+    return YES;
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    data = [[NSMutableArray alloc] init];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +76,4 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 @end
